001192e22d5937978d4bc882abc001e967fffd11
ChrisDimopoulos <diomo_chris_aigaleo@hotmail.com>
2023-10-12 15:04:33 +0300

    more

---
+++ JavaScript/Grasshopper - Personalized Message (8)/solution.js
@@ -0,0 +1,7 @@
+function greet (name, owner) {
+  return (name ==  owner) ? 'Hello boss' : 'Hello guest';
+}
+
+console.log(greet('Daniel', 'Daniel') == 'Hello boss');
+console.log(greet('Gredsdsg', 'Daniel') == 'Hello guest');
+ 
