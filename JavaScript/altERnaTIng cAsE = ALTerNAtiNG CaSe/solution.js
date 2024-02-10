ce82e3a97e12344ab7d4b31264f025f8f36f24ac
Dimochris <chrisdimo87@gmail.com>
2024-02-11 01:50:06 +0200

    more

---
+++ JavaScript/altERnaTIng cAsE = ALTerNAtiNG CaSe/solution.js
@@ -0,0 +1,9 @@
+String.prototype.toAlternatingCase = function () {
+  return this.split('').map(char => {
+        if (char === char.toLowerCase()) {
+            return char.toUpperCase();
+        } else {
+            return char.toLowerCase();
+        }
+    }).join('');
+}
